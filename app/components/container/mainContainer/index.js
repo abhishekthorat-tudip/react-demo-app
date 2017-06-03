7d96eb84c5dc1a6c86fac2e21a8fdf521f7ec5e9
Abhishek Thorat <abhishek.thorat@tudip.nl>
2017-06-04 00:06:20 +0530

    Added the blank header and footer

--- app/components/container/mainContainer/index.js
+++ app/components/container/mainContainer/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Header, Footer } from '../../ui';
 
-const MainContainer = props => <div className="parent-container">{props.children}</div>;
+const MainContainer = props =>
+  <div className="parent-container">
+    <Header />
+    {props.children}
+    <Footer />
+  </div>
 
 export { MainContainer };
---
+++ app/components/ui/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () =>
+  <div className="header">
+    <div className="header--element">image logo</div>
+    <div className="header--element">Search box</div>
+    <div className="header--element">Header tabs</div>
+  </div>
+
+export { Header }
